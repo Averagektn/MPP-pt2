bd70a314731c81dedc5e8af0b8d319c10602c54e
Glushachenko Nikita <rylonoboev2004@gmail.com>
2024-09-28 19:51:23 +0300

    web sockets started

--- lab4/TaskManager/TaskManager/app.js
+++ lab4/TaskManager/TaskManager/app.js
@@ -1,37 +1,102 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
-const path = require("path");
-const AuthMiddleware_1 = require("./middleware/AuthMiddleware");
-const admin = require("firebase-admin");
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const serviceAccount = require("./config/taskmanager-dedf9-firebase-adminsdk-uia8o-f0091c57e0.json");
-const authApp = admin.initializeApp({
+const socket_io_1 = require("socket.io");
+const http = require('http');
+/*const serviceAccount = require("./config/taskmanager-dedf9-firebase-adminsdk-uia8o-f0091c57e0.json");
+admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://taskmanager-dedf9-default-rtdb.europe-west1.firebasedatabase.app",
     storageBucket: "taskmanager-dedf9.appspot.com"
-});
-const index_1 = require("./routes/index");
-const debug = require('debug')('my express app');
+});*/
 const app = express();
-const corsOptions = cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['Authorization']
+const server = http.createServer(app);
+const io = new socket_io_1.Server(server);
+/*io.use((socket, next) => {
+    const token = socket.handshake.query.token;
+
+    //authorize();
+
+    if (token === 'valid_token') {
+        next();
+    } else {
+        const err: any = new Error('Unauthorized');
+        err.data = { content: "Please retry later" };
+        next(err);
+    }
+});*/
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+    socket.on('createTask', (data) => __awaiter(void 0, void 0, void 0, function* () {
+        //const result = await taskController.createTask(data);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('taskCreated', result);
+    }));
+    socket.on('uploadFile', (file) => __awaiter(void 0, void 0, void 0, function* () {
+        //const result = await taskController.uploadFile(file);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('fileUploaded', result);
+    }));
+    socket.on('updateTask', (taskId, data) => __awaiter(void 0, void 0, void 0, function* () {
+        //const result = await taskController.updateTask(taskId, data);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('taskUpdated', result);
+    }));
+    socket.on('deleteTask', (taskId) => __awaiter(void 0, void 0, void 0, function* () {
+        //const result = await taskController.deleteTask(taskId);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('taskDeleted', result);
+    }));
+    socket.on('filterTasks', (filter) => __awaiter(void 0, void 0, void 0, function* () {
+        //const result = await taskController.filterTasks(filter);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('tasksFiltered', result);
+    }));
+    socket.on('getTasks', () => __awaiter(void 0, void 0, void 0, function* () {
+        //const result = await taskController.getTasks();
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('tasksFetched', result);
+    }));
+    socket.on('getTotalPages', () => __awaiter(void 0, void 0, void 0, function* () {
+        //const result = await taskController.getTotalPages();
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('totalPagesFetched', result);
+    }));
+    socket.on('getTaskById', (taskId) => __awaiter(void 0, void 0, void 0, function* () {
+        //const result = await taskController.getTaskById(taskId);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('taskFetched', result);
+    }));
+    socket.on('createUser', (data) => __awaiter(void 0, void 0, void 0, function* () {
+        //const result = await authController.createUser(data);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('userCreated', result);
+    }));
+    socket.on('getAccessToken', (credentials) => __awaiter(void 0, void 0, void 0, function* () {
+        //const result = await authController.getAccessToken(credentials);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('accessTokenFetched', result);
+    }));
+    socket.on('getRefreshToken', (token) => __awaiter(void 0, void 0, void 0, function* () {
+        //const result = await authController.getRefreshToken(token);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('refreshTokenFetched', result);
+    }));
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
 });
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(corsOptions);
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.json());
-app.use(AuthMiddleware_1.default);
-app.use('/', index_1.default);
-app.options('*', corsOptions);
-app.set('port', process.env.PORT || 3000);
-const server = app.listen(app.get('port'), function () {
-    debug(`Express server listening on port ${server.address().port}`);
+const PORT = 1337;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 //# sourceMappingURL=app.js.map
--- lab4/TaskManager/TaskManager/app.ts
+++ lab4/TaskManager/TaskManager/app.ts
@@ -1,45 +1,113 @@
 import * as express from 'express';
-import { AddressInfo } from "net";
-import * as path from 'path';
 import authorize from './middleware/AuthMiddleware';
-
+import { Server } from 'socket.io';
+import taskController from './controllers/TaskController';
+import authController from './controllers/AuthController';
 import * as admin from 'firebase-admin';
 
-const cookieParser = require('cookie-parser');
-const cors = require('cors')
+const http = require('http');
 
-const serviceAccount = require("./config/taskmanager-dedf9-firebase-adminsdk-uia8o-f0091c57e0.json");
-const authApp = admin.initializeApp({
+/*const serviceAccount = require("./config/taskmanager-dedf9-firebase-adminsdk-uia8o-f0091c57e0.json");
+admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://taskmanager-dedf9-default-rtdb.europe-west1.firebasedatabase.app",
     storageBucket: "taskmanager-dedf9.appspot.com"
-});
+});*/
 
-import routes from './routes/index';
-
-const debug = require('debug')('my express app');
 const app = express();
-const corsOptions = cors({
-    origin: 'http://localhost:5173', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'], 
-    credentials: true, 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-    exposedHeaders: ['Authorization']
+const server = http.createServer(app);
+const io = new Server(server);
+
+/*io.use((socket, next) => {
+    const token = socket.handshake.query.token; 
+
+    //authorize();
+
+    if (token === 'valid_token') { 
+        next(); 
+    } else {
+        const err: any = new Error('Unauthorized');
+        err.data = { content: "Please retry later" }; 
+        next(err); 
+    }
+});*/
+
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    socket.on('createTask', async (data) => {
+        //const result = await taskController.createTask(data);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('taskCreated', result);
+    });
+
+    socket.on('uploadFile', async (file) => {
+        //const result = await taskController.uploadFile(file);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('fileUploaded', result);
+    });
+
+    socket.on('updateTask', async (taskId, data) => {
+        //const result = await taskController.updateTask(taskId, data);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('taskUpdated', result);
+    });
+
+    socket.on('deleteTask', async (taskId) => {
+        //const result = await taskController.deleteTask(taskId);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('taskDeleted', result);
+    });
+
+    socket.on('filterTasks', async (filter) => {
+        //const result = await taskController.filterTasks(filter);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('tasksFiltered', result);
+    });
+
+    socket.on('getTasks', async () => {
+        //const result = await taskController.getTasks();
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('tasksFetched', result);
+    });
+
+    socket.on('getTotalPages', async () => {
+        //const result = await taskController.getTotalPages();
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('totalPagesFetched', result);
+    });
+
+    socket.on('getTaskById', async (taskId) => {
+        //const result = await taskController.getTaskById(taskId);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('taskFetched', result);
+    });
+
+    socket.on('createUser', async (data) => {
+        //const result = await authController.createUser(data);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('userCreated', result);
+    });
+
+    socket.on('getAccessToken', async (credentials) => {
+        //const result = await authController.getAccessToken(credentials);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('accessTokenFetched', result);
+    });
+
+    socket.on('getRefreshToken', async (token) => {
+        //const result = await authController.getRefreshToken(token);
+        const result = { name: 'a', description: 'b', photo: 'c' };
+        socket.emit('refreshTokenFetched', result);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(corsOptions);
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.json());
-app.use(authorize);
-
-app.use('/', routes);
-
-app.options('*', corsOptions);
-
-app.set('port', process.env.PORT || 3000);
-
-const server = app.listen(app.get('port'), function () {
-    debug(`Express server listening on port ${(server.address() as AddressInfo).port}`);
+const PORT = 1337;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
